b230f4d3e64c4cebaa4dbb8f6f2d42eafab877fa
clauddy <claudemakarova@gmail.com>
2020-09-02 12:19:22 +0300

    added login validation

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import Vuelidate from 'vuelidate';
 import dateFilter from "./filters/date.filter.js";
+import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min.js'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuelidate);
 Vue.filter('date', dateFilter)
 
 new Vue({
