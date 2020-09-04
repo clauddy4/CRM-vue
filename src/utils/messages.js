77bb770ecc9f8821920b176184662ff9de3ac5ab
clauddy <claudemakarova@gmail.com>
2020-09-04 07:25:27 +0300

    added error logger

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,6 @@
 export default {
-    'logout': 'Вы вышли из системы'
+    'logout': 'Вы вышли из системы',
+    'auth/user-not-found': 'Пользователя с таким Email и паролем не существует',
+    'auth/wrong-password': 'Неправильный пароль',
+    'auth/email-already-in-use': 'Пользователь с таким Email уже существует'
 }
