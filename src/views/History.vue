a6edbccde37847831f02f015951d8ce9e477e578
Olga Makarova <olgamakarovatvr@gmail.com>
2020-12-24 21:51:37 +0300

    fixed date selecter

--- src/views/History.vue
+++ src/views/History.vue
@@ -4,6 +4,12 @@
       <h3>{{ 'History_Title' | localize }}</h3>
     </div>
 
+        <label>{{ 'SelectPeriod' | localize }}</label>
+        <select class="date-select" v-model="selected">
+          <option class="date-option" selected value="">{{ 'ForAllTheTimes' | localize }}</option>
+          <option v-for="date in dates" :key=date :value="date">{{month(date)}}</option>
+        </select>
+
     <div class="history-chart" :class="!records.length ? 'hidden' : ''">
       <canvas ref="canvas"></canvas>
     </div>
@@ -26,11 +32,16 @@
         :container-class="'pagination'"
         :page-class="'waves-effect'"
       />
+
+      <div class="input-field col s12">
+  </div>
+
     </section>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
 import HistoryTable from '../components/HistoryTable'
 import paginationMixin from '../mixins/pagination.mixin'
 import { Pie } from 'vue-chartjs'
@@ -46,27 +57,76 @@ export default {
   data: () => ({
     loading: true,
     records: [],
+    displayedRecords: [],
+    selected: '',
+    cats: [],
+    dates: []
   }),
   async mounted() {
     this.records = await this.$store.dispatch('fetchRecords')
-    this.records = this.records.filter(arr =>
-      arr.date.substr(5,2) == '12'
-    )
+    this.displayedRecords = this.records
     const categories = await this.$store.dispatch('fetchCategories')
+    this.cats = categories
+
+    this.selectBuilder()
 
     this.setup(categories)
-
     this.loading = false
   },
+  watch: {
+      selected() {
+        this.records = this.displayedRecords
+        if (this.selected) {
+          this.records = this.records.filter(arr => arr.date.substr(5,2) == this.selected)
+        }
+        else this.records = this.displayedRecords
+        const categories = this.$store.dispatch('fetchCategories')
+        this.setup(this.cats)
+    }
+  },
   methods: {
-    monthBuilder(month) {
-      switch(month) {
+    selectBuilder() {
+      this.records.forEach(i => this.dates.push(i.date.substr(5,2)))
+      this.dates = new Set(this.dates.reverse())
+    },
+    month(date) {
+      switch(date) {
+        case '01':
+          return localizeFilter('January')
+          break
+        case '02':
+          return localizeFilter('February')
+          break
+        case '03':
+          return localizeFilter('March')
+          break
+        case '04':
+          return localizeFilter('April')
+          break
+        case '05':
+          return localizeFilter('May')
+          break
+        case '06':
+          return localizeFilter('June')
+          break
+        case '07':
+          return localizeFilter('July')
+          break
+        case '08':
+          return localizeFilter('August')
+          break
+        case '09':
+          return localizeFilter('September')
+          break
+        case '10':
+          return localizeFilter('October')
+          break
         case '11':
-          return 'November'
-          break;
+          return localizeFilter('November')
+          break
         case '12':
-          return 'December'
-          break;
+          return localizeFilter('December')
+          break
       }
     },
     setup(categories) {
@@ -105,6 +165,11 @@ export default {
               'rgba(75, 192, 192, 0.2)',
               'rgba(153, 102, 255, 0.2)',
               'rgba(255, 159, 64, 0.2)',
+              'rgba(218, 65, 103, 0.3)',
+              'rgba(8, 61, 119, 0.2)',
+              'rgba(192, 226, 24, 0.2)',
+              'rgba(131, 169, 92, 0.2)',
+              
             ],
             borderColor: [
               'rgba(255, 99, 132, 1)',
@@ -113,6 +178,10 @@ export default {
               'rgba(75, 192, 192, 1)',
               'rgba(153, 102, 255, 1)',
               'rgba(255, 159, 64, 1)',
+              'rgba(218, 65, 103, 1)',
+              'rgba(8, 61, 119, 0.8)',
+              'rgba(192, 226, 24, 1)',
+              'rgba(131, 169, 92, 1)',
             ],
             borderWidth: 1,
           },
